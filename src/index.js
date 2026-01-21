0c06a082daad53ef20ba921e510f0ef2d5051a5d
Swayam <swayamgosavi1410@gmail.com>
2026-01-21 15:48:52 +0530

    Starter Code

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import express from 'express';
+import dotenv from 'dotenv';
+
+const app = express();
+dotenv.config();
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
