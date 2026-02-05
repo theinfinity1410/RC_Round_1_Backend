fba3b58e22dfaff2e6bd2935a9404e8c75d4d5fc
Pranjali Lad <129070814+Pranjali74@users.noreply.github.com>
2026-02-05 17:19:06 +0530

    Delete src/controllers/lifeline.route.js

--- src/controllers/lifeline.route.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { useFreezeLifeline } from "../controllers/lifeline.controller";
-import { authMiddleware } from "../middleware/auth";
-
-const router = express.Router();
-
-router.post("/freeze", authMiddleware, useFreezeLifeline);
-
-export default router;
